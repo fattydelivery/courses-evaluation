2f3428cc82b679e3c4775cbef88f0a50c4a85a3c
Bingjie Yan <44976445+beiyuouo@users.noreply.github.com>
2021-06-01 11:03:15 +0800

    修改部署信息

--- course-evaluation/README.md
+++ course-evaluation/README.md
@@ -1,3 +1,37 @@
-https://github.com/otale/tale
+# Course Evaluation
 
-https://github.com/zhyocean/MyBlog
+Evaluation courses anonymously ;)
+
+## Front-end deployment
+
+```sh
+mysql -uroot -pniit1234
+create database forum;
+
+mysql -uroot -pniit1234 -Dforum < forum.sql
+
+java -jar course-evaluation-0.0.1-SNAPSHOT.war
+```
+
+> Send email to [bj.yan.pa@qq.com](bj.yan.pa@qq.com) if you wanna built war package.
+
+<details>
+    <summary>Used package</summary>
+    - layui
+    - springboot
+    - maven
+    - mybatis
+</details>
+
+## Back-end deployment
+
+
+## Dashboard deployment
+
+
+## Reference
+
+Thanks to these awesome projects!
+
+- https://github.com/otale/tale
+- https://github.com/zhyocean/MyBlog
--- course-evaluation/src/main/resources/forum.sql
+++ course-evaluation/src/main/resources/forum.sql
@@ -11,10 +11,10 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 31/05/2021 07:46:58
+ Date: 31/05/2021 22:01:39
 */
 
-SET NAMES utf8mb4;
+SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
@@ -22,42 +22,36 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_comment`;
 CREATE TABLE `t_comment`  (
-  `comment_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `course_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `comment_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `course_id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `comment_time` datetime(0) NULL DEFAULT NULL,
-  `comment_content` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `comment_content` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`comment_id`) USING BTREE,
   INDEX `course_id`(`course_id`) USING BTREE,
   CONSTRAINT `t_comment_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `t_course` (`course_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_comment
 -- ----------------------------
-INSERT INTO `t_comment` VALUES ('3d9905a6-bd16-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 13:01:18', '刘老师棒棒！');
-INSERT INTO `t_comment` VALUES ('3d992548-bd16-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 13:01:18', '刘老师棒棒！');
-INSERT INTO `t_comment` VALUES ('3d992906-bd16-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 13:01:18', '刘老师棒棒！');
-INSERT INTO `t_comment` VALUES ('af1feafb-bd21-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 14:23:13', '我爱刘老师!');
-INSERT INTO `t_comment` VALUES ('b31a6cf7-bd21-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 14:23:20', '水水水水水');
-INSERT INTO `t_comment` VALUES ('d478b1af-bd21-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 14:24:16', '天啊');
-INSERT INTO `t_comment` VALUES ('da53f9ff-bd21-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 14:24:25', '这也太能水了');
-INSERT INTO `t_comment` VALUES ('df12f509-bd21-11eb-a45d-00e04c860bea', 'B16290', '2021-05-25 14:24:33', '现在是2021年5月25日14:24:32');
-INSERT INTO `t_comment` VALUES ('eb732ae8-bd21-11eb-a45d-00e04c860bea', 'B16291', '2021-05-25 14:24:54', '水一水这个');
-INSERT INTO `t_comment` VALUES ('f2a6e668-bd21-11eb-a45d-00e04c860bea', 'Z16342', '2021-05-25 14:25:06', '杨老师也不能放过啊');
+INSERT INTO `t_comment` VALUES ('d31ef330-c1b9-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:42:22', '大家好');
+INSERT INTO `t_comment` VALUES ('ee684a06-c1b5-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:14:29', '刘老师棒棒！');
+INSERT INTO `t_comment` VALUES ('ee684f8c-c1b5-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:14:29', '刘老师好腻害！');
+INSERT INTO `t_comment` VALUES ('ee6852e7-c1b5-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:14:29', '吹爆刘老师！');
 
 -- ----------------------------
 -- Table structure for t_course
 -- ----------------------------
 DROP TABLE IF EXISTS `t_course`;
 CREATE TABLE `t_course`  (
-  `course_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `course_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `course_id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `course_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `course_credit` int(0) NULL DEFAULT NULL,
   `course_credit_hour` int(0) NULL DEFAULT NULL,
-  `teacher_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `type_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `teacher_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`course_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_course
@@ -71,34 +65,48 @@ INSERT INTO `t_course` VALUES ('Z16342', '数据仓库与数据挖掘', 4, 64, '
 -- ----------------------------
 DROP TABLE IF EXISTS `t_like`;
 CREATE TABLE `t_like`  (
-  `like_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `course_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `like_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `course_id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `like_time` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`like_id`) USING BTREE,
   INDEX `course_id`(`course_id`) USING BTREE,
   CONSTRAINT `t_like_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `t_course` (`course_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_like
 -- ----------------------------
+INSERT INTO `t_like` VALUES ('06583f64-c1ba-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:43:47');
+INSERT INTO `t_like` VALUES ('1f12c19b-c1be-11eb-88be-00e04c860bea', 'B16291', '2021-05-31 11:13:07');
+INSERT INTO `t_like` VALUES ('37a89fa2-c1be-11eb-88be-00e04c860bea', 'B16291', '2021-05-31 11:13:48');
+INSERT INTO `t_like` VALUES ('382aae8e-c1be-11eb-88be-00e04c860bea', 'B16291', '2021-05-31 11:13:49');
+INSERT INTO `t_like` VALUES ('40e552f8-c1be-11eb-88be-00e04c860bea', 'Z16342', '2021-05-31 11:14:04');
 
 -- ----------------------------
 -- Table structure for t_rating
 -- ----------------------------
 DROP TABLE IF EXISTS `t_rating`;
 CREATE TABLE `t_rating`  (
-  `rating_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `course_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `rating_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `course_id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `rating_time` datetime(0) NULL DEFAULT NULL,
-  `rating_score` double NULL DEFAULT NULL,
+  `rating_score` int(0) NULL DEFAULT NULL,
   PRIMARY KEY (`rating_id`) USING BTREE,
   INDEX `course_id`(`course_id`) USING BTREE,
   CONSTRAINT `t_rating_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `t_course` (`course_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_rating
 -- ----------------------------
+INSERT INTO `t_rating` VALUES ('1be69042-c1ba-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:44:24', 5);
+INSERT INTO `t_rating` VALUES ('271d7ec7-c1ba-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:44:42', 5);
+INSERT INTO `t_rating` VALUES ('36d4e400-c1be-11eb-88be-00e04c860bea', 'B16291', '2021-05-31 11:13:47', 5);
+INSERT INTO `t_rating` VALUES ('38ffc51f-c1be-11eb-88be-00e04c860bea', 'B16291', '2021-05-31 11:13:50', 3);
+INSERT INTO `t_rating` VALUES ('3fbdf338-c1be-11eb-88be-00e04c860bea', 'Z16342', '2021-05-31 11:14:02', 3);
+INSERT INTO `t_rating` VALUES ('40e4b6f5-c1be-11eb-88be-00e04c860bea', 'Z16342', '2021-05-31 11:14:04', 4);
+INSERT INTO `t_rating` VALUES ('4b769a81-c1b6-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:17:05', 5);
+INSERT INTO `t_rating` VALUES ('4b773d7c-c1b6-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:17:05', 4);
+INSERT INTO `t_rating` VALUES ('7e850a73-c1ba-11eb-88be-00e04c860bea', 'B16290', '2021-05-31 10:47:09', 5);
 
 SET FOREIGN_KEY_CHECKS = 1;
